86d3362514a4aa40d3104cffb15e9092842bb24c
ВашÐOrest <_206orest122006@gmail.com>
2025-05-07 22:58:20 +0300

    Initial clean commit without secrets

--- src/firebase-admin.js
+++ src/firebase-admin.js
@@ -6,7 +6,17 @@ import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+let serviceAccount;
+
+if (process.env.FIREBASE_CONFIG) {
+  // На хостингу: читаємо з змінної середовища
+  serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+} else {
+  // Локально: читаємо з файлу
+  serviceAccount = JSON.parse(
+    await readFile(join(__dirname, '../serviceAccountKey.json'))
+  );
+}
 
 const adminApp = initializeApp({
   credential: cert(serviceAccount)
